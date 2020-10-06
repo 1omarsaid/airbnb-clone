ad8630f9c0bf96c349a383cff33956bd21aa1895
1omarsaid <omar.said@ryerson.ca>
2020-10-05 22:30:50 -0400

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p>© 2020 Airbnb clone! no rights reserved - this is a demo!</p>
+            <p>Privacy - Terms - Sitemap - Company Details</p>
+        </div>
+    )
+}
+
+export default Footer
